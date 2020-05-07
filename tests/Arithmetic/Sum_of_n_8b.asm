b856a7ae4d817df0c1f447b5cae44c2ff9dd72a7
TQ Z <ztqakita@163.com>
2020-05-07 16:33:11 +0800

    add arithmetic asm

---
+++ tests/Arithmetic/Sum_of_n_8b.asm
@@ -0,0 +1,27 @@
+data segment
+    a db 1,2,3,4,5,6,7,8,9,10
+data ends                    
+
+code segment
+    assume ds:data,cs:code
+    
+    start:
+        mov ax,data
+        mov ds,ax
+        mov cl,10
+        lea bx,a 
+        mov ah,00
+        mov al,00  
+        
+    l1:
+        add al,byte ptr[bx]
+        inc bx
+        dec cl
+        cmp cl,00
+        jnz l1
+        mov ah,4ch
+        int 21h
+        
+code ends 
+
+end start
