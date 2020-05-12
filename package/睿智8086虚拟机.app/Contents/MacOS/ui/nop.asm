3a1b47ee83588b53008a15cb0adc53ae06d446dc
Terence <i386681404@qq.com>
2020-05-12 09:45:43 +0800

    MacOS package

---
+++ package/睿智8086虚拟机.app/Contents/MacOS/ui/nop.asm
@@ -0,0 +1,34 @@
+NAME Arithmetic
+TITLE add_8b_16b
+; test passed
+
+ASSUME CS:CODE,DS:DATA
+
+DATA SEGMENT
+    num1 DB 05h
+    num2 DB 06h
+    num3 DW 1234h
+    num4 DW 0002h
+    sum DB ?
+    sum2 DW ?
+DATA ENDS  
+
+CODE SEGMENT
+    start: mov ax,data
+           mov ds,ax           ;initialize data segment
+    
+           mov al,num1
+           add al,num2         ;add the 2 bytes
+    
+           mov sum,al          ;stores result in memory
+    
+           mov cx,num3
+           add cx,num4         ;add the 2 words
+    
+           mov sum2,cx         ;stores the result in memory
+    
+           mov ah,4ch
+           int 21h
+    
+CODE ENDS
+END START
